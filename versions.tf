1a03ee839ebe079550cd0d84565058c3d7e5a38c
Keny <kenydebo@gmail.com>
2022-06-02 17:44:02 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "Omoba-Terraform-Demos"
 
     workspaces {
       name = "sentinel-example"
